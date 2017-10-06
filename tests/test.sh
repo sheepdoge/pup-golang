ed154676db7034415a1d06dc1dac7f2081c3df92
mattjmcnaughton <mattjmcnaughton@gmail.com>
2017-10-05 22:31:41 -0400

    Finish 0.1 pup golang
    This commit contains the following code to get to 0.1 for pup-golang.
    We make the following changes:
    
    - Expand the variables the user can define (i.e. `go_bins`, `gopath`,
      etc.)
    - Download go binary directly on Debian, to ensure we get a recent
      version (16.04 includes go 1.3 by default, which is much too old).
    - Ensure path entries only occur once.
    - Write unit tests.
    - Switch from using a python2.7 base docker image to Ubuntu 16.04 to
      resolve some sys.path issues with `python-apt`.

--- tests/test.sh
+++ tests/test.sh
@@ -3,15 +3,39 @@
 set -e
 
 test::check_syntax() {
-  ansible-playbook playbook.yml --syntax-check
+  ansible-playbook playbook.yml -i 'localhost' -e '{"dotfile": "~/.bashrc", "gopath": "/go", "go_bins": ["golang.org/x/tools/...", "github.com/golang/lint/golint"], "go_deleted_bins": []}' --syntax-check
 }
 
 test::run_ansible() {
-  ansible-playbook playbook.yml
+  ansible-playbook playbook.yml -i 'localhost' -e '{"dotfile": "~/.bashrc", "gopath": "/go", "go_bins": ["golang.org/x/tools/...", "github.com/golang/lint/golint"], "go_deleted_bins": []}'
 }
 
 test::assert_output() {
-  echo 'TODO'
+  . ~/.bashrc
+
+  if ! which go >/dev/null; then
+    echo 'go is not installed'
+    exit 1
+  fi
+
+  if ! echo $PATH | grep -q "$GOPATH/bin"; then
+    echo "$GOPATH/bin not included in $PATH"
+    exit 1
+  fi
+
+  if ! which golint >/dev/null; then
+    echo "golint is not installed"
+    exit 1
+  fi
+
+  # Assert when we source ~/.bashrc again, we only write $GOPATH/bin to $PATH
+  # once
+  . ~/.bashrc
+
+  if [ ":$PATH:" == "*:$GOPATH/bin:*:$GOPATH/bin:*" ]; then
+    echo "$GOPATH/bin written to $PATH to often"
+    exit 1
+  fi
 }
 
 test::check_syntax
